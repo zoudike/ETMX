fb572ea7b5b1d1b1573f9dc99940a6c9a8e0c680
杨香港 <yangxianggang@yangxianggangdeiMac.local>
2016-10-30 11:18:21 +0800

    创建项目

---
+++ ETMX/ETMX/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ETMX
+//
+//  Created by 杨香港 on 2016/10/30.
+//  Copyright © 2016年 杨香港. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ETMX/ETMX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ETMX
+//
+//  Created by 杨香港 on 2016/10/30.
+//  Copyright © 2016年 杨香港. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
