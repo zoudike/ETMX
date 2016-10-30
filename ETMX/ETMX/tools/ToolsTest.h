df0b82575f80fbbfc2b17fbf6104422207bbadbf
杨香港 <yangxianggang@yangxianggangdeiMac.local>
2016-10-30 16:36:55 +0800

    我在Library和tools文件夹下创建了文件，等下删除

---
+++ ETMX/ETMX/tools/ToolsTest.h
@@ -0,0 +1,13 @@
+//
+//  ToolsTest.h
+//  ETMX
+//
+//  Created by 杨香港 on 2016/10/30.
+//  Copyright © 2016年 杨香港. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToolsTest : NSObject
+
+@end
---
+++ ETMX/ETMX/tools/ToolsTest.m
@@ -0,0 +1,13 @@
+//
+//  ToolsTest.m
+//  ETMX
+//
+//  Created by 杨香港 on 2016/10/30.
+//  Copyright © 2016年 杨香港. All rights reserved.
+//
+
+#import "ToolsTest.h"
+
+@implementation ToolsTest
+
+@end
