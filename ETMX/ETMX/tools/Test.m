26ff4107ce20467eacbbbac443518bf55679f320
wenpq <wenpq@wenpeiqidemacbook-pro.local>
2016-10-31 23:33:27 +0800

    增加用户管理器、用户模型、任务模型、机床模型

--- ETMX/ETMX/tools/Test.m
+++ ETMX/ETMX/tools/Test.m
@@ -10,17 +10,20 @@
 #import "NetWorkManager.h"
 @implementation Test
 -(void)soapRequestTest{
-    NSArray *paramters = @[@"admin",@"admin"];
-    NSString *urlStr = @"http://192.168.1.161:8085/ETMX/services/Login";
-    NSString *methodName = @"checkUserInfo";
+    NSArray *paramters = @[@"SGS000359"];
+    NSString *urlStr = @"http://192.168.1.161:8085/ETMX/services/TaskScanExecution";
+    NSString *methodName = @"getTasksByUserCode";
+//URL:http://192.168.1.161:8085/ETMX/services/TaskScanExecution?wsdl
+
+
     [NetWorkManager sendRequestWith:paramters url:urlStr method:methodName success:^(id data) {
         NSError *error = nil;
         NSDictionary *dict = [XMLReader dictionaryForXMLData:data error:&error];
         NSDictionary *dict1 = [dict valueForKey:@"soap:Envelope"];
         NSDictionary *dict2 = [dict1 valueForKey:@"soap:Body"];
-        NSDictionary *dict3 = [dict2 valueForKey:@"ns1:checkUserInfoResponse"];
+        NSDictionary *dict3 = [dict2 valueForKey:@"ns1:getTasksByUserCodeResponse"];//该字段根据实践情况获得
         NSDictionary *dict4 = [dict3 valueForKey:@"ns1:out"];
-        NSString *str = [dict4 valueForKey:@"text"];//该字段根据实践情况获得
+        NSString *str = [dict4 valueForKey:@"text"];
         NSData *newData = [str dataUsingEncoding:NSUTF8StringEncoding];
         NSXMLParser *p = [[NSXMLParser alloc] initWithData:newData];
         [p setDelegate:self];
@@ -32,21 +35,15 @@
 }
 //以下代理是具体情况实现
 -(void)parserDidStartDocument:(NSXMLParser *)parser{
-    NSLog(@"--------BEGINPARSER---------");
 }
 -(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict{
-    NSLog(@"节点－－－%@",elementName);
-    NSLog(@"节点属性－－－－%@",attributeDict);
-    NSLog(@"%@",[attributeDict valueForKey:@"flag"]);
+    NSLog(@"节点＝＝%@",elementName);
+    
 }
 -(void)parser:(NSXMLParser *)parser foundComment:(NSString *)comment{
-    NSLog(@"节点内容－－－－%@",comment);
 }
 -(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{
-    NSLog(@"节点－－－ %@",elementName);
 }
-
 -(void)parserDidEndDocument:(NSXMLParser *)parser{
-    NSLog(@"---------ENDPARSER--------------");
 }
 @end
