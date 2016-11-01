ea055bd975ce8f3ee92315c73fa277b265d315bd
杨香港 <yangxianggang@yangxianggangdeiMac.local>
2016-11-01 15:42:16 +0800

    配置文件修改， 以及登录界面修改

--- ETMX/ETMX/ViewController.m
+++ ETMX/ETMX/ViewController.m
@@ -20,6 +20,18 @@
     // Do any additional setup after loading the view, typically from a nib.
     Test *netWorkingTest = [[Test alloc] init];
     [netWorkingTest soapRequestTest];
+    
+    
+    UILabel * titleLabel = [[UILabel alloc] init];
+    [titleLabel setText:@"欢迎使用 ETMX"];
+    [titleLabel setFont:[UIFont systemFontOfSize:60]];
+    [titleLabel setFrame:CGRectMake(0, 0, 100, 30)];
+    
+    titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:titleLabel];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:titleLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:titleLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:100]];
+
 }
 
 
